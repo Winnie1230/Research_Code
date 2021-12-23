f15e50228a70de2a59ab5e9433b9f7da161ec018
Winnie1230 <winnie1998228@gmail.com>
2021-12-23 14:26:54 +0800

    experiment output data post-processing

---
+++ Experiment/data_process_1214.m
@@ -0,0 +1,67 @@
+clear all; close all; clc;
+
+csv = load('C:\Users\BioRoLaT470S\Documents\r09522826\Experiment\Linkleg_48V\1214\1214_violentjump11.csv');
+
+%% Remove data
+% remove it if all columns are zero
+
+% mask = (data(:,1) == 0);
+data = csv(find(csv(:,1) ~= 0),:);
+
+threshold = 0.05;
+mod_poscmd1 = data(:,3);
+mod_poscmd1(mod_poscmd1 <= threshold) = 2*pi-mod_poscmd1(mod_poscmd1 <= threshold);
+mod_poscmd2 = data(:,8);
+mod_poscmd2(mod_poscmd2 <= threshold) = 2*pi-mod_poscmd2(mod_poscmd2 <= threshold);
+mod_pos1 = data(:,5);
+mod_pos1(mod_pos1 <= threshold) = 2*pi-mod_pos1(mod_pos1 <= threshold);
+mod_pos2 = data(:,10);
+mod_pos2(mod_pos2 <= threshold) = 2*pi-mod_pos2(mod_pos2 <= threshold);
+
+subplot(3,1,1);
+% plot(data(:,1),data(:,3),'Color','#0072BD','DisplayName','position cmd','LineWidth',2); hold on
+plot(data(:,1),mod_poscmd1,'Color','#0072BD','DisplayName','position cmd','LineWidth',2); hold on
+% plot(data(:,1),data(:,5),'Color','#D95319','LineStyle','--','DisplayName','encoder reply','LineWidth',2);
+plot(data(:,1),mod_pos1(:,1),'Color','#D95319','LineStyle','--','DisplayName','encoder reply','LineWidth',2);
+
+% title({['No load wheel mode'],['Forward speed: 1 rad/s, Recover speed: 5 rad/s'],['Motor1 position']},'Interpreter','latex');
+% title({['24V violent jump no contact'],['Recover speed: 100 deg/s'],['Motor1(R) position']},'Interpreter','latex');
+% title({['24V violent jump contact'],['Recover speed: 100 deg/s'],['Motor1(R) position']},'Interpreter','latex');
+% title({['24V slow motion contact'],['Forward speed: 100 deg/s, Recover speed: 100 deg/s'],['Motor1(R) position']},'Interpreter','latex');
+% title({['24V slow motion no contact'],['Forward speed: 100 deg/s, Recover speed: 100 deg/s'],['Motor1(R) position']},'Interpreter','latex');
+title({['48V slow motion no contact'],['Forward speed: 100 deg/s, Recover speed: 100 deg/s'],['Motor1(R) position']},'Interpreter','latex');
+
+xlabel('t[s]','Interpreter','latex');
+ylabel('pos[rad]','Interpreter','latex');
+legend('Location','best');
+axis([min(data(:,1)) max(data(:,1)) -0.1 2*pi+0.2])
+
+subplot(3,1,2);
+% plot(data(:,1), data(:,8),'Color','#7E2F8E','DisplayName','position cmd','LineWidth',2); hold on
+plot(data(:,1),mod_poscmd2,'Color','#7E2F8E','DisplayName','position cmd','LineWidth',2); hold on
+% plot(data(:,1),data(:,10),'Color','#EDB120','LineStyle','--','DisplayName','encoder reply','LineWidth',2);
+plot(data(:,1),mod_pos2(:,1),'Color','#EDB120','LineStyle','--','DisplayName','encoder reply','LineWidth',2);
+
+title('Motor2(L) position','Interpreter','latex');
+xlabel('t[sec]','Interpreter','latex');
+ylabel('pos[rad]','Interpreter','latex');
+legend('Location','best');
+axis([min(data(:,1)) max(data(:,1)) -0.1 2*pi+0.2])
+
+currentR_max = max(abs(data(:,7)));
+currentL_max = max(abs(data(:,12)));
+
+subplot(3,1,3);
+plot(data(:,1),data(:,7),'Color','#0072BD','DisplayName','current1(R)','LineWidth',2); hold on
+plot(data(:,1),data(:,12),'Color','#D95319','DisplayName','current2(L)','LineWidth',2);
+title('Motor current','Interpreter','latex');
+xlabel('t[sec]','Interpreter','latex');
+ylabel('current[A]','Interpreter','latex');
+legend('Location','best');
+axis([min(data(:,1)) max(data(:,1)) -15 10]);
+
+posR_err = sqrt(mean((data(:,3)-data(:,5)).^2));
+posR_var = std(abs(data(:,3)-data(:,5)));
+posL_err = sqrt(mean((data(:,8)-data(:,10)).^2));
+posL_var = std(abs(data(:,8)-data(:,10)));
+
